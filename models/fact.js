b5d183d4fc4fc7a91825aec8dc40d6f12dc99a7e
August Halverson <augusthalverson@gmail.com>
2019-10-12 18:26:27 -0500

    add more fields to schema

--- models/fact.js
+++ models/fact.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 factSchema = mongoose.Schema({
     headline: String,
     details: String,
-    creator: String,
+    contributor: String,
     vote: {
         type: Number,
         default: 0
@@ -11,7 +11,9 @@ factSchema = mongoose.Schema({
     created: {
         type: Date,
         default: Date.now
-    }
+    },
+    source_link: String,
+    source_name: String
 });
 
 module.exports = mongoose.model("Fact", factSchema);
